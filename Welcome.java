ac41069a7990b1264e89ce2050ab299a75c78057
hari-branavan-arulnathan <it25100930@my.sliit.lk>
2026-07-22 14:43:48 +0530

    Question1

---
+++ Welcome.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Welcome{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your first name: ");
+        String first_name = scanner.nextLine();
+        
+        System.out.print("Enter your last name: ");
+        String last_name = scanner.nextLine();
+
+        System.out.printf("Welcome to the Second Year %s %s!\n", first_name, last_name);
+    }
+}
